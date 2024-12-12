5f3c9298f402876debf533000d4735e13ae6600e
afsalcp <cpafsal66@gmail.com>
2024-12-12 14:44:12 +0530

    first commit

--- configs/db.js
+++ configs/db.js
@@ -1,6 +1,6 @@
 const {MongoClient} =require("mongodb")
 
-const mongo=new MongoClient("mongodb+srv://cpafsal66:TBNY5M6422gI5t63@zstore.fnj8x.mongodb.net/?retryWrites=true&w=majority&appName=zstore")
+const mongo=new MongoClient(process.env.db_url)
 
 let db=mongo.db("ztore");
 
